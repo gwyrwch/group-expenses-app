c21b3ae123d5575c6c30f344e555753596edcc88
gwyrwch <gwyrwch@gmail.com>
2020-03-26 12:42:28 +0300

    sign in/up image added and some animations

--- splitwise_web/urls.py
+++ splitwise_web/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('profile', views.profile, name='profile')
+    path('profile', views.profile, name='profile'),
+    path('sign_in_up', views.sign_in_up, name='sign_in_up')
 ]
--- splitwise_web/views.py
+++ splitwise_web/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(request):
     return render(request, 'index.html')
 
+
 def profile(request):
     return render(request, 'profile.html')
+
+
+def sign_in_up(request):
+    return render(request, 'sign_in_up.html')
